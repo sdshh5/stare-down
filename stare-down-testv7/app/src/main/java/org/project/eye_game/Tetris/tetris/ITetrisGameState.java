33ec2ddc9d540c84a82199f8550aa6d43adbe249
sdshh5 <gksrnr8502@gmail.com>
2020-11-15 15:28:34 +0900

    remove stare-down-testv7 folder

--- stare-down-testv7/app/src/main/java/org/project/eye_game/Tetris/tetris/ITetrisGameState.java
+++
@@ -1,9 +0,0 @@
-package org.project.eye_game.Tetris.tetris;
-
-public interface ITetrisGameState {
-
-    void addRemoveLineCount(int line);
-    void ClearBoard();
-    void update();
-    void updateHighScore();
-}
--- stare-down-testv7/app/src/main/java/org/project/eye_game/Tetris/tetris/TetrisInitState.java
+++
@@ -1,8 +0,0 @@
-package org.project.eye_game.Tetris.tetris;
-
-public class TetrisInitState extends TetrisGameState {
-        public TetrisInitState(Tetris tetris) {
-        TetrisLog.d("TetrisInitState()");
-        this.tetris = tetris;
-    }
-}
