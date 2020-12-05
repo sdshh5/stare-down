f5504ffab9d17878af54c2609fa214388907ca56
wyuinche <wyuinche.mido@gmail.com>
2020-12-05 21:04:42 +0900

    v21

---
+++ stare-down-testv21/app/src/main/java/org/project/eye_game/Tetris/tetris/ZTetrominos.java
@@ -0,0 +1,29 @@
+package org.project.eye_game.Tetris.tetris;
+
+public class ZTetrominos extends Tetrominos {
+    public ZTetrominos() {
+        this.block = new int[][][]{
+                {
+                        {1, 1, 0},
+                        {0, 1, 1},
+                        {0, 0, 0},
+                },
+                {
+                        {0, 1, 0},
+                        {1, 1, 0},
+                        {1, 0, 0},
+                },
+        };
+        numOfBlockType = 2;
+        x = 4;
+        y = 0;
+        r = 0;
+        w = 3;
+        h = 3;
+        type = 7;
+    }
+
+    public int[][] getBlock() {
+        return block[r];
+    }
+}
---
+++ stare-down-testv21/app/src/main/java/org/project/eye_game/Tetris/tetrisgame/PlayerScoreImpl.java
@@ -0,0 +1,27 @@
+package org.project.eye_game.Tetris.tetrisgame;
+
+
+import org.project.eye_game.Tetris.tetris.Score;
+
+public class PlayerScoreImpl extends Score {
+    private int additionalScore;
+
+    protected void  calculatorScore(int removedLineCount) {
+        if (removedLineCount == 0) {
+            this.additionalScore = 1;
+        }
+
+        int lineScore[] = { 0, 88, 188, 288, 8888 };
+
+        additionalScore *= 10;
+
+        if (additionalScore > 10000) {
+            additionalScore = 10000;
+        }
+        
+        addScore(removedLineCount * 10 * additionalScore + lineScore[removedLineCount]);
+    }
+    protected void ClearBoard() {
+        addScore(100000);
+    }
+}
