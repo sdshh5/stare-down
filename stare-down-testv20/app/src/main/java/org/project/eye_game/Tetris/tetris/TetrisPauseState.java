cb3ee78991a3ef049a5919066f87bf5fd62c1e58
wyuinche <wyuinche.mido@gmail.com>
2020-12-10 12:37:05 +0900

    remove previous versions

--- stare-down-testv20/app/src/main/java/org/project/eye_game/Tetris/tetris/TetrisPauseState.java
+++
@@ -1,9 +0,0 @@
-package org.project.eye_game.Tetris.tetris;
-
-public class TetrisPauseState extends TetrisGameState {
-    public TetrisPauseState(Tetris tetris) {
-        this.tetris = tetris;
-    }
-
-    public boolean isPauseState() { return true; }
-}
--- stare-down-testv21/app/src/main/java/org/project/eye_game/Tetris/player/PlayerObserver.java
+++
@@ -1,8 +0,0 @@
-package org.project.eye_game.Tetris.player;
-
-public interface PlayerObserver {
-
-    void update();
-    void startGame();
-
-}
